f16cbc316315702db3b4fa1b8517a1cb82e197cd
JoeBlack220 <348653205@qq.com>
2021-12-03 22:49:24 -0800

    feat: finish webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -3,12 +3,33 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
+    
     // Your code here.
+    TCPSocket sock;
+    sock.connect(Address(host, "http"));
+    
+    // Build the HTTP Request
+    ostringstream stream;
+    stream << "GET " << path << " HTTP/1.1\r\n";
+    stream <<  "Host: " << host << "\r\n";
+    stream << "Connection: close\r\n\r\n";
 
+    // Send the HTTP Request
+    auto req = stream.str();
+    sock.write(req);
+
+    ostringstream output;
+    while(!sock.eof()) {
+        output << sock.read();
+    }
+
+    cout << output.str();
+    sock.shutdown(SHUT_RDWR);
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -17,8 +38,9 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
+    
 }
 
 int main(int argc, char *argv[]) {
