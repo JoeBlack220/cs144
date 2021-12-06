048ff3e038d1b6ca977984653906343450ff1fe8
JoeBlack220 <348653205@qq.com>
2021-12-05 16:04:10 -0800

    feat: finish wrapping_integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -7,15 +8,16 @@
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
-
+const uint32_t bitmask_1 = 0xffffffffUL;
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t tmp = (n&bitmask_1) + isn.raw_value();
+    uint32_t raw_value = tmp&bitmask_1;
+    return WrappingInt32{raw_value};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,16 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t diff = n - wrap(checkpoint, isn);
+    uint64_t res;
+    
+    // To avoid overflow
+    // diff > UINT32_MAX/2 -> might be a negative diff
+    // checkpoint > res, must be a negative diff 
+    if (diff > UINT32_MAX/2 && checkpoint > checkpoint - (1ULL << 32) + diff) {
+        res = checkpoint - (1ULL << 32) + diff;
+    } else {
+        res = checkpoint + diff;
+    }
+    return res;
 }
